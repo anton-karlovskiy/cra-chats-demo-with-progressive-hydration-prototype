a0fc3560af8293a9cbf96c352778ff9301106c16
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-02-18 13:13:20 +0300

    feat: adopted concurrent mode in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+
+// This is a setup function that simulates client/server setup.
+// On the server, it simulates Sidebar and Content being available right away.
+// On the client, it simulates them resolving over network (when you press a button).
+export default function getApp(isSimulatingNetwork) {
+  let Sidebar;
+  let Content;
+  if (isSimulatingNetwork) {
+    // Artificially delay the components, as if we fetch code over network.
+    Sidebar = React.lazy(
+      () =>
+        new Promise(resolve => {
+          // Triggered from buttons in public/index.html
+          window.resolveSidebar = () => {
+            resolve({ default: RealSidebar });
+          };
+        })
+    );
+    Content = React.lazy(
+      () =>
+        new Promise(resolve => {
+          // Triggered from buttons in public/index.html
+          window.resolveContent = () => {
+            resolve({ default: RealContent });
+          };
+        })
+    );
+  } else {
+    // On the server, we have the components ready right away.
+    Sidebar = RealSidebar;
+    Content = RealContent;
+  }
+
+  function RealSidebar() {
+    const [count, setCount] = React.useState(0);
+    const [didHydrate, setDidHydrate] = React.useState(false);
+    React.useEffect(() => {
+      // Make it easy to see when the code has loaded.
+      setDidHydrate(true);
+    }, []);
+
+    return (
+      <div
+        style={{
+          background: didHydrate
+            ? "rgba(0, 255, 0, 0.1)"
+            : "rgba(255, 0, 0, 0.1)",
+          border: "1px solid grey",
+          margin: 10,
+          padding: 10
+        }}>
+        <h1>
+          Sidebar ({didHydrate ? "Hydrated with JS" : "Initial HTML"})
+        </h1>
+        <button onClick={() => setCount(c => c + 1)}>
+          Clicked on sidebar {count} times
+        </button>
+      </div>
+    );
+  }
+
+  function RealContent() {
+    const [count, setCount] = React.useState(0);
+    const [didHydrate, setDidHydrate] = React.useState(false);
+    React.useEffect(() => {
+      // Make it easy to see when the code has loaded.
+      setDidHydrate(true);
+    }, []);
+
+    return (
+      <div
+        style={{
+          background: didHydrate
+            ? "rgba(0, 255, 0, 0.1)"
+            : "rgba(255, 0, 0, 0.1)",
+          border: "1px solid grey",
+          margin: 10,
+          padding: 10
+        }}>
+        <h1>
+          Content ({didHydrate ? "Hydrated with JS" : "Initial HTML"})
+        </h1>
+        <button onClick={() => setCount(c => c + 1)}>
+          Clicked on content {count} times
+        </button>
+      </div>
+    );
+  }
+
+  function App() {
+    return (
+      <div
+        className="App"
+        style={{
+          border: "1px solid grey",
+          margin: 10,
+          padding: 10
+        }}>
+        <h1>React Progressive Hydration Demo*</h1>
+        <h3>
+          <i>
+            * very experimental — likely contains bugs.
+          </i>
+        </h3>
+        <h2>
+          This app is server-rendered to HTML.{" "}Concurrent Mode lets us hydrate parts of UI without waiting for <i>all</i> JS to load.
+        </h2>
+        <React.Suspense fallback={<h2>Loading sidebar...</h2>}>
+          <Sidebar />
+        </React.Suspense>
+        <br />
+        <React.Suspense fallback={<h2>Loading content...</h2>}>
+          <Content />
+        </React.Suspense>
+      </div>
+    );
+  }
+
+  return App;
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactDOMServer from "react-dom/server";
+import getApp from "./App";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Our app component tree looks like this:
+// - App
+//    - Sidebar
+//    - Content
+//
+// Our initial HTML contains them all.
+// However, on the client, we'll load their code piece by piece.
+//
+// The point of this demo is to show that in Concurrent Mode,
+// React can make Sidebar/Content interactive the moment their
+// code arrives--without waiting for the whole JS bundle to load.
+
+// Do a full server render to fill the HTML.
+doServerRender();
+
+// Start hydration as soon as we have the App component.
+// Note this doesn't *need* Sidebar or Content code to load yet.
+doClientRender();
+
+function doServerRender() {
+  const App = getApp(
+    // Pretend we're on the server, and have all the code
+    // to render the app (including Content and Sidebar).
+    false
+  );
+  const rootElement = document.getElementById(
+    "root"
+  );
+  // Just fill the initial HTML. As if it's server rendered.
+  rootElement.innerHTML = ReactDOMServer.renderToString(
+    <App />
+  );
+}
+
+function doClientRender() {
+  const App = getApp(
+    // Make Content and Sidebar code unavailable on first
+    // client-side render, as if they're still loading over network.
+    true
+  );
+  const rootElement = document.getElementById(
+    "root"
+  );
+  // Start hydrating the app. Note that in Concurrent Mode,
+  // we don't need *all* code to be loaded. Notice how if
+  // you press "Load Sidebar JS code", you can interact
+  // with the Sidebar even though the Content component
+  // hasn't loaded its code yet.
+  ReactDOM.createRoot(rootElement, {
+    hydrate: true
+  }).render(<App />);
+}
